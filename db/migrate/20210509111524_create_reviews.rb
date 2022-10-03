2125fa87036a62a28b605595ea1ee343bfd870e2
SallyMulupi <mulupisally2gmail.com>
2022-10-03 21:48:18 +0300

    done

--- db/migrate/20210509111524_create_reviews.rb
+++ db/migrate/20210509111524_create_reviews.rb
@@ -1,12 +1,14 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
-  def change
-    create_table :reviews do |t|
-      t.string :username
-      t.string :comment
-      t.integer :rating
-      t.belongs_to :dog_house, null: false, foreign_key: true
+def change
+  create_table :reviews do |t|
+    t.string :username
+    t.string :comment
+    t.integer :rating
 
-      t.timestamps
-    end
+    # foreign_key: true establishes a relationship between a review and a dog house
+    t.belongs_to :dog_house, null: false, foreign_key: true
+
+    t.timestamps
   end
+end
 end
